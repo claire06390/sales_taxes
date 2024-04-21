88860a2136ddf108f8e7cfa0731d18141be0daf4
cmarini <claire.marini@avisto.com>
2024-04-21 18:33:48 +0200

    init project

---
+++ src/test/java/com/example/sales_taxes/SalesTaxesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sales_taxes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesTaxesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
